b2745817f2ef9064515ecc885b2f4b6207afee4e
Renatoomagat <renatoomagat@mail.ru>
2023-04-22 21:34:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_4_Colors
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is Green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is Cyan!");
+            }
+        }
+    }
+}
